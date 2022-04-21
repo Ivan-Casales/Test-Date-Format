af54889a95a9cec59ca00417d5fec697f9615994
Ivan Casales <casalesivan@hotmail.com>
2022-04-21 17:17:23 -0300

    parte 1 y 2 hechas_1.0

--- Test/Library_Test/ChangeFormatTests.cs
+++ Test/Library_Test/ChangeFormatTests.cs
@@ -6,17 +6,32 @@ namespace DateFormat.Test
     public class Tests
     {
         [Test]
-        public void Test1()
+        public void TestChangeFormat1_1()
         {
-            string NewDate = Program.ChangeFormat("10/11/1977");
+            string NewDate = Program.ChangeFormat1("10/11/1977");
             Assert.AreEqual(NewDate, "1977-11-10");
         }
 
         [Test]
-        public void Test2()
+        public void TestChangeFormat1_2()
         {
-            string NewDate = Program.ChangeFormat("09/02/2005");
+            string NewDate = Program.ChangeFormat1("09/02/2005");
             Assert.AreEqual(NewDate, "2005-02-09");
         }
+
+        [Test]
+        public void TestChangeFormat2_1()
+        {
+            string NewDate = Program.ChangeFormat2("10/11/1977");
+            Assert.AreEqual(NewDate, "10-11-1977");
+        }
+
+        [Test]
+        public void TestChangeFormat2_2()
+        {
+            string NewDate = Program.ChangeFormat2("09/02/2005");
+            Assert.AreEqual(NewDate, "09-02-2005");
+        }
+
     }
 }
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -11,15 +11,22 @@ namespace DateFormat
 {
     public class Program
     {
-        public static String ChangeFormat(String year)
+        public static String ChangeFormat1(String year)
         {
             return year.Substring(6) + "-" + year.Substring(3, 2) + "-" + year.Substring(0, 2);
         }
 
+
+        public static String ChangeFormat2(String year)
+        {
+            return year.Substring(0, 2) + "-" + year.Substring(3, 2) + "-" + year.Substring(6);
+        }
+
+
         public static void Main(string[] args)
         {
             string testDate = "10/11/1977";
-            Console.WriteLine("{0} se convierte a: {1} ", testDate, ChangeFormat(testDate));
+            Console.WriteLine("{0} se convierte a: {1} ", testDate, ChangeFormat1(testDate));
         }
     }
 }
